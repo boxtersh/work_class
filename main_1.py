ecf246a5d5831603baa09d84faa8c8d72ad51413
Yuriy Terlyanskiy <boxtersh@yandex.ru>
2025-06-05 16:10:27 +0300

    feat (create class Animal) задача N1

--- main_1.py
+++ main_1.py
@@ -1,1 +1,22 @@
-# Привет
+class Animal:
+    def __init__(self, name:str, type:str, age:str):
+
+        self.name = name
+        self.type = type
+        self.age = age
+
+    def animal_sound(self, sound:str, power_sound:str):
+
+        self.sound = sound
+        self.power_sound = power_sound
+
+        print(f'Звук животного: {self.sound}\nСила звука животного: {self.power_sound}')
+
+
+
+animal = Animal('Мурка', 'Кошка', '7')
+print(f'Имя животного: {animal.name}, Вид животного: {animal.type}, Возраст животного: {animal.age}')
+
+animal.animal_sound('sound', '15%')
+
+
